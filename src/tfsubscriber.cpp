1620d349b7fa04d2abe24054dee7b97ac2542559
James Bell <jtb2013@gmail.com>
2017-08-18 17:43:19 -0500

    Began fixing tfsubscriber.cpp

--- src/tfsubscriber.cpp
+++ src/tfsubscriber.cpp
@@ -2,6 +2,8 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
+
+#include <cstring>
 #include "quaternion.h"
 #include "device.h"
 
@@ -12,49 +14,7 @@ Vec3 tarAtt;
 Vec3 loc;
 Vec3 att;
 
-void messageCallback( const geometry_msgs::TransformStamped& t){
 
-  String a =  t.header.frame_id;
-  //Set target
-  //Vec4 x;
-  if(a=="tar"){
-    tarLoc[0] = t.transform.translation.x;
-    tarLoc[1] = t.transform.translation.y;
-    tarLoc[2] = t.transform.translation.z;
-    x.v[0] = t.transform.rotation.w;
-    x.v[1] = t.transform.rotation.x;
-    x.v[2] = t.transform.rotation.y;
-    x.v[3] = t.transform.rotation.z;
-    tarAtt = Quat2RPY(x);
-    logmsg(t.transform.translation);
-  }
-  else{
-    loc[0] = t.transform.translation.x;
-    loc[1] = t.transform.translation.y;
-    loc[2] = t.transform.translation.z;
-    x.v[0] = t.transform.rotation.w;
-    x.v[1] = t.transform.rotation.x;
-    x.v[2] = t.transform.rotation.y;
-    x.v[3] = t.transform.rotation.z;
-    att = Quat2RPY(x);
-  }
-  //edit formulas!
-  vx = (float)(tarLoc[0] - loc[0]);
-
-  vy = (float)(tarLoc[1] - loc[1]);
-
-  theta = (float)att[2];//yaw
-
-  thetaROC  = (float)(tarAtt[2]-att[2]);
-
-  sendMessage();
-
-}
-int main(int argc, char **argv){
-  ros::init(argc,argv,"omnibot_throttle");
-  ros::Subscriber sub = nh.subscribe("/vicon/omnibot", 1000, messageCallback);
-  ros::spinOnce();
-}
 void sendMessage(){
   radio.write( &vx, sizeof(float) );
   radio.write( &vy, sizeof(float) );
@@ -86,6 +46,45 @@ void sendMessage(){
     }
   }
 }
+void messageCallback( const geometry_msgs::TransformStamped& t){
+
+  string a =  t.header.frame_id;
+  //Set target
+  //Vec4 x;
+  Vec4 quat;
+  if(a=="tar"){
+    tarLoc.v[0] = t.transform.translation.x;
+    tarLoc.v[1] = t.transform.translation.y;
+    tarLoc.v[2] = t.transform.translation.z;
+    quat.v[0] = t.transform.rotation.w;
+    quat.v[1] = t.transform.rotation.x;
+    quat.v[2] = t.transform.rotation.y;
+    quat.v[3] = t.transform.rotation.z;
+    tarAtt = Quat2RPY(quat);
+    logmsg(t.transform.translation);
+  }
+  else{
+    loc.v[0] = t.transform.translation.x;
+    loc.v[1] = t.transform.translation.y;
+    loc.v[2] = t.transform.translation.z;
+    quat.v[0] = t.transform.rotation.w;
+    quat.v[1] = t.transform.rotation.x;
+    quat.v[2] = t.transform.rotation.y;
+    quat.v[3] = t.transform.rotation.z;
+    att = Quat2RPY(quat);
+  }
+  //edit formulas!
+  vx = (float)(tarLoc.v[0] - loc.v[0]);
+
+  vy = (float)(tarLoc.v[1] - loc.v[1]);
+
+  theta = (float)att.v[2];//yaw
+
+  thetaROC  = (float)(tarAtt.v[2]-att.v[2]);
+
+  sendMessage();
+
+}
 void logmsg(float a){
   char log_msg[50];
   float temp = a;
@@ -93,4 +92,10 @@ void logmsg(float a){
   sprintf(log_msg, "Float = %0d.%d", (int)temp, temp1);
   nh.loginfo(log_msg);
 
+}
+
+int main(int argc, char **argv){
+  ros::init(argc,argv,"omnibot_throttle");
+  ros::Subscriber sub = nh.subscribe("/vicon/omnibot", 1000, messageCallback);
+  ros::spinOnce();
 }
